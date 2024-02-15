e3c4796353e7b2350694001aff9453342b0efb42
Esmi92 <mendoza.esmii92@gmail.com>
2024-02-14 19:36:53 -0600

    frase nueva

--- src/pages/components/Quote.js
+++ src/pages/components/Quote.js
@@ -6,8 +6,8 @@ export default function Quote() {
     <div className="QuoteDiv">
       <img src={logo} className="QuoteLogo" alt="logo" />
       <div>
-        <p className="QuoteText">"Lo mejor que puedes aprender en la vida es a amar y a ser amado de regreso"</p>
-        <p className="QuoteAuthor">Anonimo</p>
+        <p className="QuoteText">Cada Familia tiene una historia <br/> <b>Bienvenidos a la nuestra</b></p>
+        <p className="QuoteAuthor"></p>
       </div>
     </div>
   )
